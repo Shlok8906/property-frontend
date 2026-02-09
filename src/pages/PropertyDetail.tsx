d0fd4b10185936cb53d28fa73f7802d047195992
Shlok8906 <psofficial7789@gmail.com>
2026-02-09 19:43:30 +0530

    Corrected grid layout to display three properties per row in PropertyDetail.tsx

--- src/pages/PropertyDetail.tsx
+++ src/pages/PropertyDetail.tsx
@@ -248,7 +248,7 @@ export default function PropertyDetail() {
           </div>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
           {/* Main Content */}
           <div className="lg:col-span-2 space-y-8">
             {/* Image Gallery */}
